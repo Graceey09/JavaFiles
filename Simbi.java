5bb67f9a9abae91b546931fd382265544dbb6c4b
Graceey09 <gracemaduekwe95@gmail.com>
2023-05-24 18:56:44 +0100

    JavaFiles

---
+++ Simbi.java
@@ -0,0 +1,8 @@
+public class Simbi {
+
+	public static void main(String[] args){
+	System.out.printf("%s, %d, %.2f %n", "Hello I am Simbi", 15, 23.56);
+	
+	} 
+	
+ }
---
+++ TomideBigHead.java
@@ -0,0 +1,11 @@
+public class TomideBigHead {
+
+	public static void main(String[] args) {
+	 
+	 String result = "result";
+	 double number = 9.5;
+	 System.out.printf("%s%n%f", result,number);
+	 System.out.printf("%s%n%f%n%b", "solomon",11.6, true);
+	}
+
+}
